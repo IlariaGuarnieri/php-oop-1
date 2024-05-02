7f91ccc009de7612ee6e2727051c5ee8828ee9a1
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-05-02 13:40:10 +0100

    [add]  first commit

---
+++ Model/Movie.php
@@ -0,0 +1,15 @@
+<?php
+class Movie{
+  private $name;
+  public $subtitle;
+  private $description;
+  public $director;
+
+  public function __construct(string $_title,)
+  {
+    this->name = $_name
+  }
+
+}
+
+?>
