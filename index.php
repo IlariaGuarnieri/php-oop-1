a1e059698c63b0632169a53797c0d5c04215630a
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-05-03 09:08:21 +0100

    [add] minuscole

--- index.php
+++ index.php
@@ -2,19 +2,19 @@
 require_once __DIR__ . '/Model/Movie.php';
 
 // inizializzo nuova istanza
-$Barbie = new Movie('Barbie', 'un bel film', 1990, ['luca', 'mario','carla']);
-$Barbie->subtitle= 'Fantasia';
-$Barbie->director= 'Greta Gerwig';
+$barbie = new Movie('Barbie', 'un bel film', 1990, ['luca', 'mario','carla']);
+$barbie->subtitle= 'Fantasia';
+$barbie->director= 'Greta Gerwig';
 
-$Gossip_girl = new Movie ('Gossip Girl', 'gossip, ragazze', 2010,['lucia', 'marco','mirco']);
-$Gossip_girl->subtitle= 'Adolescenza';
-$Gossip_girl->director= 'Amy Heckerling';
+$gossip_girl = new Movie ('Gossip Girl', 'gossip, ragazze', 2010,['lucia', 'marco','mirco']);
+$gossip_girl->subtitle= 'Adolescenza';
+$gossip_girl->director= 'Amy Heckerling';
 
-$Re_leone = new Movie('Il Re Leone','classici disney',2005, ['simba', 'nala', 'mufasa']);
-$Re_leone->subtitle= 'Il cerchio della vita';
-$Re_leone->director= 'Walt Disney';
-// var_dump($Barbie);
-// var_dump($Gossip_girl);
+$re_leone = new Movie('Il Re Leone','classici disney',2005, ['simba', 'nala', 'mufasa']);
+$re_leone->subtitle= 'Il cerchio della vita';
+$re_leone->director= 'Walt Disney';
+// var_dump($barbie);
+// var_dump($gossip_girl);
 
 require __DIR__ . '/Partials/Head.php'
 ?>
@@ -34,12 +34,12 @@ require __DIR__ . '/Partials/Head.php'
 <div class="cont_card d-flex mx-3">
 <div class="card" style="width: 18rem;">
   <div class="card-body">
-  <h2><?php $Barbie->getName() ?></h2>
-  <p><?php $Barbie->getDescription() ?></p>
-  <p><?php $Barbie->getEcho() ?></p>
+  <h2><?php $barbie->getName() ?></h2>
+  <p><?php $barbie->getDescription() ?></p>
+  <p><?php $barbie->getEcho() ?></p>
   <ul>
     <h4>Attori:</h4>
-    <?php foreach ($Barbie->actors as $actor) : ?>
+    <?php foreach ($barbie->actors as $actor) : ?>
       <li>
         <?php echo $actor ?>
       </li>
@@ -50,12 +50,12 @@ require __DIR__ . '/Partials/Head.php'
 
 <div class="card" style="width: 18rem;">
   <div class="card-body">
-  <h2><?php $Gossip_girl->getName() ?></h2>
-  <p><?php $Gossip_girl->getDescription() ?></p>
-  <p><?php $Gossip_girl->getEcho() ?></p>
+  <h2><?php $gossip_girl->getName() ?></h2>
+  <p><?php $gossip_girl->getDescription() ?></p>
+  <p><?php $gossip_girl->getEcho() ?></p>
   <ul>
     <h4>Attori:</h4>
-    <?php foreach ($Gossip_girl->actors as $actor) : ?>
+    <?php foreach ($gossip_girl->actors as $actor) : ?>
       <li>
         <?php echo $actor ?>
       </li>
@@ -66,12 +66,12 @@ require __DIR__ . '/Partials/Head.php'
 
 <div class="card" style="width: 18rem;">
   <div class="card-body">
-  <h2><?php $Re_leone->getName() ?></h2>
-  <p><?php $Re_leone->getDescription() ?></p>
-  <p><?php $Re_leone->getEcho() ?></p>
+  <h2><?php $re_leone->getName() ?></h2>
+  <p><?php $re_leone->getDescription() ?></p>
+  <p><?php $re_leone->getEcho() ?></p>
   <ul>
     <h4>Attori:</h4>
-    <?php foreach ($Re_leone->actors as $actor) : ?>
+    <?php foreach ($re_leone->actors as $actor) : ?>
       <li>
         <?php echo $actor ?>
       </li>
